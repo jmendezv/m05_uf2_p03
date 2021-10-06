9b5491c824d0b632fa95fdbfbe29212781ede42e
pep <jmendez1@xtec.cat>
2021-10-06 18:24:15 +0200

    Added support for print testing

--- src/main/kotlin/cat/copernic/jmendezv/Main.kt
+++ src/main/kotlin/cat/copernic/jmendezv/Main.kt
@@ -1,4 +1,30 @@
 package cat.copernic.jmendezv
 
-fun suma(a: Int, b: Int) = a + b
-fun producto(a: Int, b: Int) = a * b
+import java.time.LocalDate
+
+fun sumaYMultiplica(a: Int, b: Int) = Pair(a + b, a * b)
+
+fun sayHi() {
+    println("Hi world!")
+}
+
+fun ordenar(array: Array<Int>) {
+    println(array.sorted())
+}
+
+// @ParameterizedTest
+// @MethodSource
+fun soloPares(lista: List<Int>): List<Int> = TODO("Homework")
+
+// @ParameterizedTest
+// @MethodSource
+fun restaYDvision(a: Double, b: Double): Pair<Double, Double> = TODO("Homework")
+
+// com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+fun printDiaDeLaSemana(fecha: LocalDate): Unit = TODO("Homework")
+
+// com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+fun printEdad(fechaNacimiento: LocalDate): Unit = TODO("Homework")
+
+
+//fun paresOrdenados(a: Array<Int>) = a.filter { it % 2 == 0 }.sorted().toTypedArray()
--- src/test/kotlin/cat/copernic/jmendezv/MainKtTest.kt
+++ src/test/kotlin/cat/copernic/jmendezv/MainKtTest.kt
@@ -1,5 +1,7 @@
 package cat.copernic.jmendezv
 
+
+import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
 import org.junit.jupiter.api.*
 
 import org.junit.jupiter.api.Assertions.*
@@ -8,64 +10,233 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
+//@DisplayName("Testing demo")
+//@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 internal class MainKtTest {
 
-    companion object {
-
-        @BeforeAll
-        @JvmStatic
-        fun init() {
-            println("In @BeforeAll")
-        }
-
-        @AfterAll
-        @JvmStatic
-        fun end() {
-            println("In @AfterAll")
-        }
-
-        @JvmStatic
-        fun provideParametersForSuma(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(3, 4),
-                Arguments.of(5, 1),
-                Arguments.of(0, 4),
-                Arguments.of(1, 9),
-            )
-        }
-
-        @JvmStatic
-        fun provideParametersForProducto(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(3, 4),
-                Arguments.of(5, 1),
-                Arguments.of(0, 4),
-                Arguments.of(1, 9),
-            )
-        }
-
+    @Test
+    fun sayHiTest() {
+        val output: String = tapSystemOut { sayHi() }
+        assertEquals("Hi world!", output.trim())
     }
 
-    @BeforeEach
-    fun setUp() {
-        println("In setup")
+    @Test
+    fun ordernaTest() {
+        val output: String = tapSystemOut { ordenar(arrayOf(2,1,4,3)) }
+        assertEquals("[1, 2, 3, 4]", output.trim())
     }
 
-    @AfterEach
-    fun tearDown() {
-        println("In tearDown")
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideParametersForSuma")
-    fun sumaTest(a: Int, b: Int) {
-        assertEquals(a + b, cat.copernic.jmendezv.suma(a, b))
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideParametersForProducto")
-    fun productoTest(a: Int, b: Int) {
-        assertEquals(a * b, cat.copernic.jmendezv.producto(a, b))
+    @Test
+    fun sumaYMultiplicaTest() {
+        assertEquals(Pair(7, 12), sumaYMultiplica(3,4))
     }
 
 }
+
+
+//
+//    @Nested
+//    @DisplayName("General")
+//    class General {
+//
+//        companion object {
+//
+//            @BeforeAll
+//            @JvmStatic
+//            fun init() {
+//                println("In @BeforeAll")
+//            }
+//
+//            @AfterAll
+//            @JvmStatic
+//            fun end() {
+//                println("In @AfterAll")
+//            }
+//
+//            @JvmStatic
+//            fun provideParametersForSuma(): Stream<Arguments> {
+//                return Stream.of(
+//                    Arguments.of(3, 4),
+//                    Arguments.of(5, 1),
+//                    Arguments.of(0, 4),
+//                    Arguments.of(1, 9),
+//                )
+//            }
+//
+//            @JvmStatic
+//            fun provideParametersForProducto(): Stream<Arguments> {
+//                return Stream.of(
+//                    Arguments.of(3, 4),
+//                    Arguments.of(5, 1),
+//                    Arguments.of(0, 4),
+//                    Arguments.of(1, 9),
+//                )
+//            }
+//
+//            @JvmStatic
+//            fun provideParametersForOrdena(): Stream<Arguments> {
+//                return Stream.of(
+//                    Arguments.of(arrayOf(2, 4), arrayOf(2, 3, 1, 4)),
+//                    Arguments.of(arrayOf(2, 4, 6), arrayOf(6, 4, 2, 3, 1, 7, 5)),
+//                    Arguments.of(arrayOf(2, 4, 8), arrayOf(2, 8, 5, 3, 1, 4)),
+//                    Arguments.of(arrayOf(12, 14, 18), arrayOf(12, 18, 51, 3, 21, 14)),
+//                    Arguments.of(arrayOf(22, 24, 38), arrayOf(22, 38, 45, 31, 11, 24)),
+//                )
+//            }
+//
+//        }
+//
+//        @BeforeEach
+//        fun setUp() {
+//            println("In setup")
+//        }
+//
+//        @AfterEach
+//        fun tearDown() {
+//            println("In tearDown")
+//        }
+//
+//        @DisplayName("Suma test")
+//        @Order(4)
+//        @ParameterizedTest
+//        @MethodSource("provideParametersForSuma")
+//        fun sumaTest(a: Int, b: Int) {
+//            println("sumaTest")
+//            assertEquals(a + b, cat.copernic.jmendezv.suma(a, b))
+//        }
+//
+//        @DisplayName("Producto test")
+//        @Order(3)
+//        @ParameterizedTest
+//        @MethodSource("provideParametersForProducto")
+//        fun productoTest(a: Int, b: Int) {
+//            assertEquals(a * b, cat.copernic.jmendezv.producto(a, b))
+//        }
+//
+//        @DisplayName("Ordena test")
+//        @Order(2)
+//        @ParameterizedTest
+//        @MethodSource("provideParametersForOrdena")
+//        fun ordenaTest(a: Array<Int>, b: Array<Int>) {
+//            assertArrayEquals(a, paresOrdenados(b))
+//        }
+//
+//        @Tag("output")
+//        @DisplayName("Output test")
+//        @Order(1)
+//        @Test
+//        fun sayHiTest() {
+//            println("sayTest")
+//            val output = tapSystemOut {
+//                sayHi()
+//            }
+//            assertEquals(
+//                "Hi!",
+//                output
+//            )
+//        }
+//
+//        @Nested
+//        @DisplayName("Topic")
+//        class Topic {
+//
+//            companion object {
+//
+//                @BeforeAll
+//                @JvmStatic
+//                fun init() {
+//                    println("In @BeforeAll")
+//                }
+//
+//                @AfterAll
+//                @JvmStatic
+//                fun end() {
+//                    println("In @AfterAll")
+//                }
+//
+//                @JvmStatic
+//                fun provideParametersForSuma(): Stream<Arguments> {
+//                    return Stream.of(
+//                        Arguments.of(3, 4),
+//                        Arguments.of(5, 1),
+//                        Arguments.of(0, 4),
+//                        Arguments.of(1, 9),
+//                    )
+//                }
+//
+//                @JvmStatic
+//                fun provideParametersForProducto(): Stream<Arguments> {
+//                    return Stream.of(
+//                        Arguments.of(3, 4),
+//                        Arguments.of(5, 1),
+//                        Arguments.of(0, 4),
+//                        Arguments.of(1, 9),
+//                    )
+//                }
+//
+//                @JvmStatic
+//                fun provideParametersForOrdena(): Stream<Arguments> {
+//                    return Stream.of(
+//                        Arguments.of(arrayOf(2, 4), arrayOf(2, 3, 1, 4)),
+//                        Arguments.of(arrayOf(2, 4, 6), arrayOf(6, 4, 2, 3, 1, 7, 5)),
+//                        Arguments.of(arrayOf(2, 4, 8), arrayOf(2, 8, 5, 3, 1, 4)),
+//                        Arguments.of(arrayOf(12, 14, 18), arrayOf(12, 18, 51, 3, 21, 14)),
+//                        Arguments.of(arrayOf(22, 24, 38), arrayOf(22, 38, 45, 31, 11, 24)),
+//                    )
+//                }
+//
+//            }
+//
+//            @BeforeEach
+//            fun setUp() {
+//                println("In setup")
+//            }
+//
+//            @AfterEach
+//            fun tearDown() {
+//                println("In tearDown")
+//            }
+//
+//            @DisplayName("Suma test")
+//            @Order(4)
+//            @ParameterizedTest
+//            @MethodSource("provideParametersForSuma")
+//            fun sumaTest(a: Int, b: Int) {
+//                println("sumaTest")
+//                assertEquals(a + b, cat.copernic.jmendezv.suma(a, b))
+//            }
+//
+//            @DisplayName("Producto test")
+//            @Order(3)
+//            @ParameterizedTest
+//            @MethodSource("provideParametersForProducto")
+//            fun productoTest(a: Int, b: Int) {
+//                assertEquals(a * b, cat.copernic.jmendezv.producto(a, b))
+//            }
+//
+//            @DisplayName("Ordena test")
+//            @Order(2)
+//            @ParameterizedTest
+//            @MethodSource("provideParametersForOrdena")
+//            fun ordenaTest(a: Array<Int>, b: Array<Int>) {
+//                assertArrayEquals(a, paresOrdenados(b))
+//            }
+//
+//            @Tag("output")
+//            @DisplayName("Output test")
+//            @Order(1)
+//            @Test
+//            fun sayHiTest() {
+//                println("sayTest")
+//                val output = tapSystemOut {
+//                    sayHi()
+//                }
+//                assertEquals(
+//                    "Hi!",
+//                    output
+//                )
+//            }
+//
+//        }
+//
+//    }
